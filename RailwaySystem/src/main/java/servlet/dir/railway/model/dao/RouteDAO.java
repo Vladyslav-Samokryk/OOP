cb1873d6238f1cc04cff480f7c67d2566dc81515
Vladyslav-Samokryk <43633541+Vladyslav-Samokryk@users.noreply.github.com>
2021-06-15 20:20:14 +0300

    Lab 1. Servlets

---
+++ RailwaySystem/src/main/java/servlet/dir/railway/model/dao/RouteDAO.java
@@ -0,0 +1,10 @@
+package servlet.dir.railway.model.dao;
+
+import aloyevets.epam.project_4.model.entity.Route;
+
+public interface RouteDAO {
+	Iterable<Route> findRoutes(Route route); /* Find the route by departure and
+	                                          * destination stations, departure
+	                                          * time.
+	                                          */
+}
---
+++ RailwaySystem/src/main/java/servlet/dir/railway/model/dao/TrainDAO.java
@@ -0,0 +1,7 @@
+package servlet.dir.railway.model.dao;
+
+import aloyevets.epam.project_4.model.entity.Train;
+
+public interface TrainDAO {
+	Train findTrain(Train train);
+}
